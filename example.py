60d4fb69b1f71ca849ab91db5f4a2f9ea01e2c90
Chandramouli Gnanasambandham <cgnanasa@itm.uni-stuttgart.de>
2016-09-29 20:09:06 +0200

    added required documentation

--- example.py
+++ example.py
@@ -3,6 +3,7 @@ import numpy as np
 from scipy import integrate
 import matplotlib.pyplot as plt
 from WaveEquation1D import WaveEquation1D as Wave1D
+from scipy.signal import chirp
 
 def angreifPunkte(x,mue,sigma):
     tmp= -(x-mue)**2/(2*sigma**2)
@@ -11,20 +12,25 @@ def angreifPunkte(x,mue,sigma):
 
 def main():
     n=102-2
+    t_end= 10
     tmp= np.linspace(0,1,n+2)
+
     #L= np.zeros([n,1])
     L= angreifPunkte(tmp[1:-1],0.2,0.05)
 
-    WaveObj= Wave1D(n+2,1,1,1,0.01,L)
-
     amp= 100
-    in_force= lambda t: (t>0.1)*amp*np.sin(2*np.pi*0.5*t)
-    in_time= np.arange(0, 1.0, 0.01)
+    #in_force= lambda t: (t>0.1)*amp*np.sin(2*np.pi*0.5*t)
+    in_force= lambda t: amp*chirp(t,f0=0.5,f1=10,t1=t_end,method='linear')
+    in_time= np.linspace(0, t_end, 5e3)
+
+    #plt.plot(in_time,in_force(in_time))
+    #plt.show()
 
     x0= np.zeros([n*2])
     #x0[0:n:1]= np.sin(np.pi*tmp[1:n+1:1])
-    WaveObj.solve(in_force,in_time,x0,plot_flag=1)
 
+    WaveObj= Wave1D(n+2,1,1,1,0.01,L)
+    WaveObj.solve(in_force,in_time,x0,plot_flag=1)
 
 if __name__ == '__main__':
     main()
